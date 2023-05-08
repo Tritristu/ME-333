164134fd636bfd6712668340d799433bae8c21f1
Tritristu <Tritristu@users.noreply.github.com>
2023-05-08 09:13:20 -0700

    Added Mass conservation Velocities

--- Bernoulli Lab/DataAnalysis.py
+++ Bernoulli Lab/DataAnalysis.py
@@ -11,8 +11,13 @@ trialThree = pd.read_csv('Bernoulli Lab\Trial3.csv')
 density = 998.23 # units kg/m**3 at 20C
 gravity = 9.81 # units m/s**2
 
+# Area Calculations
+trialOne['Area (m^2)'] = (np.pi/4)*(trialOne['Duct Diameter (mm)']*1e-3)**2
+trialTwo['Area (m^2)'] = (np.pi/4)*(trialTwo['Duct Diameter (mm)']*1e-3)**2
+trialThree['Area (m^2)'] = (np.pi/4)*(trialThree['Duct Diameter (mm)']*1e-3)**2
+
 # Volume Flow rate Calculations
-avgTime1 = (-1)/3
+avgTime1 = (53+60+59)/3
 avgTime2 = (116+116)/3
 avgTime3 = (19.17+18.63+19.09)/3
 
@@ -20,21 +25,26 @@ volFlux1 = 6e-3/avgTime1
 volFlux2 = 6e-3/avgTime2
 volFlux3 = 2e-3/avgTime3
 
-# print('Trial 1 Volumetric Flux:',avgTime1)
-# print('Trial 2 Volumetric Flux:',avgTime2)
-# print('Trial 3 Average Volumetric Flux:',avgTime3)
+print('Trial 1 Volumetric Flux:',avgTime1)
+print('Trial 2 Volumetric Flux:',avgTime2)
+print('Trial 3 Average Volumetric Flux:',avgTime3)
 
 # Bernoulli Velocity
+
+
 
 # Mass Conservation velocity
 massFLux1 = density*volFlux1
 massFLux2 = density*volFlux2
 massFLux3 = density*volFlux3
 
+trialOne['Mass Conservation Velocity (m/s)'] = massFLux1/(density*trialOne['Area (m^2)'])
+trialTwo['Mass Conservation Velocity (m/s)'] = massFLux1/(density*trialTwo['Area (m^2)'])
+trialThree['Mass Conservation Velocity (m/s)'] = massFLux1/(density*trialThree['Area (m^2)'])
 
 # Plotting
 
-sns.lineplot(data=trialOne,x='Duct Distance(mm)',y='Static Head (mm)')
+# sns.lineplot(data=trialOne,x='Duct Distance(mm)',y='Static Head (mm)')
 
 
 
