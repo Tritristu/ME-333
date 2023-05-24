61d6fdfd3ba6820a390c821c618cc2dacc946e8a
Tritristu <Tritristu@users.noreply.github.com>
2023-05-24 10:22:39 -0700

    started line fits

--- Minor Loss Lab/dataAnalysis.py
+++ Minor Loss Lab/dataAnalysis.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+from scipy.stats import linregress
 import seaborn as sns
 import matplotlib.pyplot as plt
 
@@ -24,9 +25,19 @@ cappedTee['Pressure Drop [Pa] (R_e = 25000)'] = 133.322*cappedTee['Pressure Drop
 cappedTee['Pressure Drop [Pa] (R_e = 35000)'] = 133.322*cappedTee['Pressure Drop [Torr] (R_e = 35000)']
 
 # Linear Regression
-# def trendline(data):
-#     later problem
+def trendline(data):
+    positions = np.concatenate([data['Position [m]'][0:5],data['Position [m]'][7:10]])
+    reynolds15000 = np.concatenate([data['Pressure Drop [Pa] (R_e = 15000)'][0:5],data['Pressure Drop [Pa] (R_e = 15000)'][7:10]])
+    reynolds25000 = np.concatenate([data['Pressure Drop [Pa] (R_e = 25000)'][0:5],data['Pressure Drop [Pa] (R_e = 25000)'][7:10]])
+    reynolds35000 = np.concatenate([data['Pressure Drop [Pa] (R_e = 35000)'][0:5],data['Pressure Drop [Pa] (R_e = 35000)'][7:10]])
+    slope1, intercept1, R, P, Err = linregress(positions, reynolds15000)
+    slope2, intercept2, R, P, Err = linregress(positions, reynolds25000)
+    slope3, intercept3, R, P, Err = linregress(positions, reynolds35000)
+    fitLines = [[slope1,intercept1],[slope2,intercept2],[slope2,intercept2]]
+    return fitLines
 
+roundElbowFits = trendline(roundElbow)
+cappedTeeFits = trendline(cappedTee)
 
 # Plotting
 sns.set_theme()
@@ -36,6 +47,10 @@ plt.title('Round Elbow Pressure Change vs Position')
 sns.scatterplot(data=roundElbow,x='Position [m]',y='Pressure Drop [Pa] (R_e = 15000)')
 sns.scatterplot(data=roundElbow,x='Position [m]',y='Pressure Drop [Pa] (R_e = 25000)')
 sns.scatterplot(data=roundElbow,x='Position [m]',y='Pressure Drop [Pa] (R_e = 35000)')
+# for lines in roundElbowFits:
+#     pressureDrops = lines[0]*roundElbow['Position [m]'] + lines[1]
+#     plt.plot(roundElbow['Position [m]'],pressureDrops)
+# plt.legend(labels=['R_e=15000','R_e=25000','R_e=35000','Fitted (R_e=15000)','Fitted (R_e=25000)','Fitted (R_e=35000)'],loc='lower right')
 plt.legend(labels=['R_e=15000','R_e=25000','R_e=35000'],loc='lower right')
 plt.ylabel('Pressure Change [Pa]')
 
